77dfb556e8174bfc86e6e2df2b350a2568f4ed9d
민재로 <111555810+Minjaero@users.noreply.github.com>
2023-11-11 21:06:38 +0900

    컴포즈4

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,64 +1,59 @@
-package com.example.jetpackcomposepractice
+package com.example.jetpackpractice4
 
-import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import com.example.jetpackcomposepractice.ui.theme.JetPackComposePracticeTheme
+import com.example.jetpackpractice4.ui.theme.JetpackPractice4Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-               Box(modifier = Modifier
-                   .background(color = androidx.compose.ui.graphics.Color.Green)
-                   .fillMaxWidth()
-                   .height(200.dp),
-               ){
-                   Text(text = "Hello")
-                   Box(
-                       modifier = Modifier
-                           .fillMaxSize()
-                           .padding(16.dp),
-                       contentAlignment = Alignment.BottomEnd,
-                       ){
-                       Text(text = "12345~~~!!!")
-                   }
+            val scrollState = rememberScrollState()
+
+
+            Column(
+                modifier = Modifier
+                    .background(color = Color.Green)
+                    .fillMaxWidth()
+                    .verticalScroll(scrollState)
+            )
+            {
+                for (i in 1..50){
+                    Text(text = "글씨 $i")
                     
                 }
             }
         }
     }
+}
 
 @Composable
-fun Greeting(name: String){
-    Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
 }
+
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview(){
-    JetPackComposePracticeTheme {
-        Greeting("김민재")
+fun GreetingPreview() {
+    JetpackPractice4Theme {
+        Greeting("Android")
     }
 }
-
-
