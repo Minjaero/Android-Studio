eb88828d88201336fd4fbb88e46d32b07a1b45f1
민재로 <111555810+Minjaero@users.noreply.github.com>
2024-05-21 22:14:13 +0900

    Button Toast

--- MainActivity.kt
+++ MainActivity.kt
@@ -1,59 +1,80 @@
-package com.example.jetpackpractice4
+package com.example.practice
 
 import android.os.Bundle
+import android.service.autofill.OnClickAction
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.jetpackpractice4.ui.theme.JetpackPractice4Theme
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.practice.ui.theme.PracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
-            val scrollState = rememberScrollState()
+            PracticeTheme {
+                MyBtn()
 
-
-            Column(
-                modifier = Modifier
-                    .background(color = Color.Green)
-                    .fillMaxWidth()
-                    .verticalScroll(scrollState)
-            )
-            {
-                for (i in 1..50){
-                    Text(text = "글씨 $i")
-                    
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyBtn(){
+
+    val context = LocalContext.current
+
+    Button(onClick = {
+        Log.d("Main" , "OnCLick")
+        Toast.makeText(context, "클릭 완료" , Toast.LENGTH_LONG).show()
+    },
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Yellow,
+            contentColor = Color.Black
+
+
+        ), modifier = Modifier
+            .width(200.dp)
+            .height(100.dp)
+
+        ) 
+    
+        {
+            Text(
+                text = "I love you you love me I love you you love me",
+                lineHeight = 30.sp,
+                fontSize = 30.sp,
+                color = Color.Black
+
+            )
+
+
+        }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    JetpackPractice4Theme {
-        Greeting("Android")
+    PracticeTheme {
+        MyBtn()
     }
 }
